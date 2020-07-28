06dda7047f6ef3bdf977dd106a53f3d05e114d2d
Kaytotes <me@kaytotes.online>
2020-07-28 01:07:03 +0100

    Build Script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "Building mpq-packer..."
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    build="linux-x64"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    build="win-x64"
+fi
+
+dotnet restore mpq-packer.sln
+
+dotnet publish mpq-packer.csproj -c Release --self-contained -r $build -o Build -f netcoreapp3.1 -p:PublishSingleFile=true
+
+cp libs/StormLib.dll Build/StormLib.dll
+cp libs/StormLibSharp.dll Build/StormLibSharp.dll
